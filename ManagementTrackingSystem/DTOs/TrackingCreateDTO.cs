3b17712161d936b0b23b789a77b82d0964e48fd2
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-29 11:29:10 +0100

    refact(DBContext): Implement data seeding setup.

--- ManagementTrackingSystem/DTOs/TrackingCreateDTO.cs
+++ ManagementTrackingSystem/DTOs/TrackingCreateDTO.cs
@@ -6,10 +6,13 @@ namespace ManagementTrackingSystem.DTOs
     {
         [Required]
         public int OrderId { get; set; }
+
         [Required]
         public string Carrier { get; set; }
+
         [Required]
         public DateTime EstimatedDeliveryDate { get; set; }
+
         [Required]
         public string TrackingNumber { get; set; }
     }
---
+++ ManagementTrackingSystem/Data/ManagementTrackingSystemDBContext.cs
@@ -0,0 +1,41 @@
+﻿using ManagementTrackingSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManagementTrackingSystem.Data
+{
+    public class ManagementTrackingSystemDBContext : DbContext
+    {
+        public ManagementTrackingSystemDBContext(DbContextOptions<ManagementTrackingSystemDBContext> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed Membership Tiers
+            modelBuilder.Entity<MembershipTier>().HasData();
+
+            // Seed Customer Data
+            modelBuilder.Entity<Customer>().HasData();
+
+            // Seed Address Data
+            modelBuilder.Entity<Address>().HasData();
+
+            // Seed Category Data
+            modelBuilder.Entity<Category>().HasData();
+
+            // Seed Product Data
+            modelBuilder.Entity<Product>().HasData();
+
+            // Seed Order Data
+            modelBuilder.Entity<Order>().HasData();
+
+            // Seed OrderItem Data
+            modelBuilder.Entity<OrderItem>().HasData();
+
+            // Seed ShippingDetail Data
+            modelBuilder.Entity<TrackingDetail>().HasData();
+
+
+        }
+    }
+}
