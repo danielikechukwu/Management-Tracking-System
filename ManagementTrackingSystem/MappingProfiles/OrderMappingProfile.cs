cb98bf99d10efb4190d7a2b24b1c28ddd0dfedcf
Daniel ikechukwu <daniel.ikechukwu@avosoft.co.uk>
2025-05-29 23:14:56 +0100

    feat(automapper): Implemented an automapping setup.

---
+++ ManagementTrackingSystem/MappingProfiles/OrderMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ManagementTrackingSystem.DTOs;
+using ManagementTrackingSystem.Models;
+
+namespace ManagementTrackingSystem.MappingProfiles
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile() {
+
+            //-----------------------------------------------------------------
+            // 1. Order -> OrderDTO
+            //-----------------------------------------------------------------
+            CreateMap<Order, OrderDTO>()
+                // Different property names: "Id" -> "OrderId"
+                .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.Id));
+                ;
+        }
+    }
+}
