79c2703fb46057834c7b5eb95650561cd19cd418
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-30 10:46:04 +0100

    feat(automapper): Implement automapper for Order to OrderDTO.

--- ManagementTrackingSystem/MappingProfiles/OrderMappingProfile.cs
+++ ManagementTrackingSystem/MappingProfiles/OrderMappingProfile.cs
@@ -12,9 +12,28 @@ namespace ManagementTrackingSystem.MappingProfiles
             // 1. Order -> OrderDTO
             //-----------------------------------------------------------------
             CreateMap<Order, OrderDTO>()
+                // Format "OrderDate" from DateTime to a string (e.g., '2025-02-06 13:15:00')
+                .ForMember(dest => dest.OrderDate, opt => opt.MapFrom(src => src.OrderDate.ToString("yyyyy-MM-dd HH:mm:ss")))
+                // Combine two properties into one
+                .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => $"{src.Customer.FirstName} {src.Customer.LastName}"))
+                // Map "Customer.PhoneNumber" -> "CustomerPhoneNumber"
+                .ForMember(dest => dest.CustomerPhoneNumber, opt => opt.MapFrom(src => src.Customer.PhoneNumber))
+                // Map "ShippingAddres" (typo in entity) -> "ShippingAddress" (DTO)
+                .ForMember(dest => dest.ShippingAddress, opt => opt.MapFrom(src => src.ShippingAddres))
+                // Format "ShippedDate" if not null
+                .ForMember(dest => dest.ShippedDate, opt => opt.MapFrom(src => src.ShippedDate.HasValue ? src.ShippedDate.Value.ToString("yyyyy-MM-dd HH:mm:ss") : null))
+                // Map "Customer.Email" -> "CustomerEmail" (different property names)
+                .ForMember(dest => dest.CustomerEmail, opt => opt.MapFrom(src => src.Customer.Email))
                 // Different property names: "Id" -> "OrderId"
                 .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.Id));
-                ;
+
+            // Note: We do NOT explicitly map .ForMember(dest => dest.TrackingDetail, ...)
+            // or .ForMember(dest => dest.OrderItems, ...) here because:
+            //  - The property names are the same in source and destination
+            //  - We have separate mappings for TrackingDetail -> TrackingDetailDTO
+            //    and OrderItem -> OrderItemDTO (see below)
+            // AutoMapper will automatically use those mappings, as long as
+            // the source and destination property names and types align.
         }
     }
 }
